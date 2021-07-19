b34ff6740fc1f81286ed65f1ffd866c0243936e6
devpro2k <78656171+devpro2k@users.noreply.github.com>
2021-07-18 21:43:24 -0700

    Update curtemp-s.js

--- final/js/curtemp-s.js
+++ final/js/curtemp-s.js
@@ -1,21 +1,25 @@
+async function getCurrTemp(city) {
+  const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=466dca7cca93940a33882268fd25bd99`;
+  return fetch(weatherURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
 
-const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&appid=466dca7cca93940a33882268fd25bd99';
-fetch(weatherURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    
-    let t = parseFloat(jsObject.main.temp);
-    let s = parseFloat(jsObject.wind.speed);
-    let output = "N/A";
-    document.getElementById('weather').innerHTML = jsObject.weather[0].description;
-    document.getElementById('current-temp').innerHTML = Math.round(t) + "&#8457;";
-    if (t <= 50 && s >= 3) {
-      let f = (35.74 + (0.6215 * t)) - (35.75 * (Math.pow(s, 0.16))) + (0.4275 * (t * (Math.pow(s, 0.16))));
-      output = Math.round(f);
-    };
-    document.getElementById("output").innerHTML = output + "&#8457;";
-    document.getElementById('humidity').innerHTML = jsObject.main.humidity + "&#37;";
-    document.getElementById('speed').innerHTML = Math.round(s) + " mph";
+      let t = parseFloat(jsObject.main.temp);
+      let city = jsObject.name;
+      let s = parseFloat(jsObject.wind.speed);
+      let output = "N/A";
+      document.getElementById('weather').innerHTML = jsObject.weather[0].description;
+      document.getElementById('current-temp').innerHTML = Math.round(t) + "&#8457;";
+      if (t <= 50 && s >= 3) {
+        let f = (35.74 + (0.6215 * t)) - (35.75 * (Math.pow(s, 0.16))) + (0.4275 * (t * (Math.pow(s, 0.16))));
+        output = Math.round(f);
+      };
+      document.getElementById("output").innerHTML = output + "&#8457;";
+      document.getElementById('humidity').innerHTML = jsObject.main.humidity + "&#37;";
+      document.getElementById('speed').innerHTML = Math.round(s) + " mph";
+      document.getElementById('cname').innerHTML = city;
+      return jsObject.coord;
+    })
+}
 
-  });
-
+export default getCurrTemp;
