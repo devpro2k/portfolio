408bac9720cb984f864e4481f16b299021a20969
devpro2k <78656171+devpro2k@users.noreply.github.com>
2021-07-18 21:38:26 -0700

    Update sevenday-s.js

--- final/js/sevenday-s.js
+++ final/js/sevenday-s.js
@@ -1,31 +1,45 @@
-const sevendayURL ="https://api.openweathermap.org/data/2.5/onecall?lat=40.45302953104331&lon=-111.89674448805302&exclude=hourly&units=imperial&appid=466dca7cca93940a33882268fd25bd99";
-fetch(sevendayURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-    /*
-     *  I am creating a variable then filtering the forecast array looking for the time 18:00:00:00
-     *  I am also using an 'includes' in my filter (Referred ta as functional using filters)
-     */
-    //const forecast = jsObject.list.filter((x) => x.dt_txt.includes("18:00:00")); ***To Delete
-    const forecast = jsObject.daily;
-    const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    for (let day = 1; day < forecast.length; day++) {
-      const d = new Date(0);
-d.setUTCSeconds(forecast[day].dt);
-      //const d = new Date(forecast[day].dt_txt);
-      const imagesrc =
-        "https://openweathermap.org/img/w/" +
-        forecast[day].weather[0].icon +
-        ".png";
-      const desc = forecast[day].weather[0].description;
+export default async function forecast({
+  lat,
+  lon
+}) {
+  const sevendayURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&units=imperial&appid=466dca7cca93940a33882268fd25bd99`;
 
-      document.getElementById(`dayofweek${day}`).textContent =
-        weekdays[d.getDay()];
-      document.getElementById(`forecast${day}`).textContent = Math.round(
-        forecast[day].temp.day
-      );
-      document.getElementById(`icon${day}`).setAttribute("src", imagesrc);
-      document.getElementById(`icon${day}`).setAttribute("alt", desc);
-    }
-  });
+  return fetch(sevendayURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+
+      /*
+       *  I am creating a variable then filtering the forecast array looking for the time 18:00:00:00
+       *  I am also using an 'includes' in my filter (Referred ta as functional using filters)
+       */
+      //const forecast = jsObject.list.filter((x) => x.dt_txt.includes("18:00:00")); ***To Delete
+      const forecast = jsObject.daily;
+      const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+      const container = document.querySelector('[data-type="sevenday-container"]');
+      container.innerHTML = "";
+      for (let day = 1; day < forecast.length; day++) {
+        const d = new Date(0);
+        d.setUTCSeconds(forecast[day].dt);
+        //const d = new Date(forecast[day].dt_txt);
+        const imagesrc =
+          "https://openweathermap.org/img/w/" +
+          forecast[day].weather[0].icon +
+          ".png";
+        const desc = forecast[day].weather[0].description;
+        const temp = forecast[day].temp.day;
+        let div = document.createElement('div'); 
+        div.classList =  "forecast-day";
+        div.innerHTML = `<p>${weekdays[day-1]}</p>
+        <figure>
+        <img src="${imagesrc}">
+        </figure>
+        <p>${desc}</p>
+        <p>${temp}&#8457;</p>
+        `;
+        container.appendChild(div);
+      }
+    })
+    .catch(e => {
+      console.log(e)
+    });
+}
